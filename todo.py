1354a1e5d6f6dac354289644379468ddbf16915a
Andrew Chao <andrew.c.chao@ischool.berkeley.edu>
2012-11-07 12:16:36 -0800

    finished multiuser login
    multi user login works

--- todo.py
+++ todo.py
@@ -37,22 +37,45 @@ def teardown_request(exception):
 def home():
     return render_template('home.html')
 
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        if(request.form['username'] == "" or request.form['password'] == "" or request.form['firstname']  == "" or request.form['lastname'] == ""):
+            error = "invalid form"
+            return render_template('register.html', error=error)
+
+        g.db.execute('insert into users (username, password, firstname, lastname) values (?, ?, ?, ?)',
+                            [request.form['username'], request.form['password'], request.form['firstname'], request.form['lastname']])
+        g.db.commit()
+        flash('successfully registered!')
+        session['logged_in'] = True
+        session['username'] = request.form['username']
+        return redirect(url_for('show_list'))
+
+    return render_template('register.html', error=error)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
+        aha = None
         cur = g.db.execute('select username, password from users where username = ? and password = ?', [request.form['username'], request.form['password']])
-        if cur <= 0:
+        if not cur.fetchall():
             error = 'Invalid username or password'
         else:
             session['logged_in'] = True
+            session['username'] = request.form['username']
             flash('You were logged in')
             return redirect(url_for('show_list'))
-    return render_template('list.html', error=error)
+        return render_template('login.html', error=error)
+
+    return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    session.pop('username', None)
     flash('You were logged out')
     return redirect(url_for('home'))
 
@@ -62,7 +85,8 @@ def about():
 
 @app.route("/show_list/")
 def show_list():
-    return
+    username = session.get('username')
+    return render_template('list.html',username=username)
 
 if __name__ == "__main__":
     app.run();
